be8766d19c87d40c34ec56ee209f2a869e08e9d7
gaugaura <gaurav.2.gaurav@nokia.com>
2025-08-09 16:01:20 +0530

    adding the test operation correction again

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -3,7 +3,7 @@ from src.maths_operations import add,sub
 
 def test_test():
     assert add(2,5) ==7
-    assert add(-2,-5) == 7
+    assert add(-2,-5) == -7
     
 def test_sub():
     assert sub(2,5) == 3
